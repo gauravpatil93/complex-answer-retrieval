1fbba6c3efbf695a965210f799c93fc152e0611d
Shilpa Dhagat <shilpa.dhagat15@gmail.com>
2017-04-03 15:33:00 -0400

    removed hardcoded values

--- entitylinking.run.sh
+++ entitylinking.run.sh
@@ -4,10 +4,10 @@ echo "============================================================="
 echo "Generating results file with entity linking............      "
 echo "============================================================="
 
-python tc_generate_entitylinking_results.py all.test200.cbor.outlines all.test200.cbor.paragraphs output_entitylinking.run enhanced
+python tc_generate_entitylinking_results.py $1 $2 $3 enhanced
 
 echo "============================================================="
 echo "Running evaluation framework on results with entity linking  "
 echo "============================================================="
 
-python eval_framework.py all.test200.cbor.hierarchical.qrels output_entitylinking.run
+python eval_framework.py $1 $2
