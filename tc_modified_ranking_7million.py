17259bb25fe9eee84635a91af0eb0f9596a66e45
Gaurav Patil <gauravpatil93@outlook.com>
2017-04-25 18:06:28 -0400

    Test bench setup

--- tc_modified_ranking_7million.py
+++ tc_modified_ranking_7million.py
@@ -4,18 +4,17 @@ import re
 from nltk.corpus import stopwords
 from stemming.porter2 import stem
 from trec_car.read_data import *
+import gc
 
 """
-This class contains methods that generate the structure that is used by the ranking functions.
 @author: Gaurav Patil.
 """
 
 
 class PartialRanking:
-    stop_words = stopwords.words('english')
     cache_words = dict()
 
-    def __init__(self, outline_file, paragraph_file, no_passages_to_extract, enable_cache=False):
+    def __init__(self, outline_file, paragraph_file, no_passages_to_extract):
         """
         Constructor
         :param outline_file: path of the outline file
@@ -24,8 +23,8 @@ class PartialRanking:
         self.passages_extract = no_passages_to_extract
         self.outline_file = outline_file
         self.paragraph_file = paragraph_file
-        self.enable_cache = enable_cache
         self.pages = self.gather_pages()
+        self.stop_words = stopwords.words('english')
 
     def gather_pages(self):
         """
@@ -38,20 +37,25 @@ class PartialRanking:
 
     def gather_paragraphs(self):
         """
-        Gets the paragraphs from the cbor
-        :return: dict of dicts ( id maps to ranked dict of words ) 
+        Modified for running 
         """
         id_to_text_dict = dict()
         with open(self.paragraph_file, 'rb') as f:
+            counter = 0
+            inner_counter = 0
             for p in itertools.islice(iter_paragraphs(f), 0, self.passages_extract):
-                id_to_text_dict[p.para_id] = Ranking.process_text_query(p.get_text())
-        if self.enable_cache is True:
-            print(" Paragraph cache created successfully ")
-            _pickle.dump(id_to_text_dict, open(os.path.join(os.curdir, "cache/paragraph_structure"), "wb"))
-        return id_to_text_dict
+                id_to_text_dict[p.para_id] = self.process_text_query(p.get_text())
+                counter += 1
+                if counter == 70000:
+                    name_of_file = "para_collection" + str(inner_counter)
+                    _pickle.dump(id_to_text_dict, open(os.path.join(os.curdir, "merge_cache/"+name_of_file), "wb"))
+                    print("Created pickle dump for collection named " + name_of_file)
+                    id_to_text_dict.clear()
+                    inner_counter += 1
+                    counter = 0
+                    gc.collect()
 
-    @staticmethod
-    def process_text_query(input_text: str):
+    def process_text_query(self, input_text: str):
         """
         Runs text processing on a given text
         :param input_text: String
@@ -62,7 +66,7 @@ class PartialRanking:
         # Remove special characters from the string
         input_text_to_lower = re.sub('[^a-zA-Z0-9 \n]', '', input_text_to_lower)
         # Remove common words using list of stop words
-        filtered_words_list = [word for word in input_text_to_lower.split() if word not in Ranking.stop_words]
+        filtered_words_list = [word for word in input_text_to_lower.split() if word not in self.stop_words]
         # Stem the list of words
         filtered_words_list = [stem(word) for word in filtered_words_list]
         # Word ranking
@@ -76,18 +80,13 @@ class PartialRanking:
 
     def gather_queries(self):
         """
-        Gathers the queries
-        :return: query_structure (query_id_plain, query_id_formatted, Ranking.process_text_query(query_id_plain) list 
+        Modified for running
         """
         query_tup_list = []
         for page in self.pages:
             for section_path in page.flat_headings_list():
                 query_id_plain = " ".join([page.page_name] + [section.heading for section in section_path])
                 query_id_formatted = "/".join([page.page_id] + [section.headingId for section in section_path])
-                tup = (query_id_plain, query_id_formatted, PartialRanking.process_text_query(query_id_plain))
+                tup = (query_id_plain, query_id_formatted, self.process_text_query(query_id_plain))
                 query_tup_list.append(tup)
-        if self.enable_cache is True:
-            print(" Query cache created successfully ")
-            _pickle.dump(query_tup_list, open(os.path.join(os.curdir, "cache/query_structure_cache"), "wb"))
-        return query_tup_list
-
+        _pickle.dump(query_tup_list, open(os.path.join(os.curdir, "cache/query_structure_cache"), "wb"))
