b45f2dedd5cc60f9947f46d3acf7f211aa08a499
Gaurav Patil <gauravpatil93@outlook.com>
2017-05-05 22:06:55 -0400

    Test Files

--- MultiThreadedTest.py
+++ MultiThreadedTest.py
@@ -3,54 +3,48 @@ from Test_Class import Test
 
 
 def func1():
-    t_object = Test(0, 1)
+    t_object = Test(10, 21)
     t_object.run_ranking()
 
 
 def func2():
-    t_object = Test(1, 2)
+    t_object = Test(21, 31)
     t_object.run_ranking()
 
 
 def func3():
-    t_object = Test(2, 3)
+    t_object = Test(31, 41)
     t_object.run_ranking()
 
 
 def func4():
-    t_object = Test(3, 4)
+    t_object = Test(41, 51)
     t_object.run_ranking()
 
 
 def func5():
-    t_object = Test(4, 5)
+    t_object = Test(51, 61)
     t_object.run_ranking()
 
 
 def func6():
-    t_object = Test(5, 6)
+    t_object = Test(61, 71)
     t_object.run_ranking()
 
 
 def func7():
-    t_object = Test(6, 7)
+    t_object = Test(71, 81)
     t_object.run_ranking()
 
 
 def func8():
-    t_object = Test(7, 8)
+    t_object = Test(81, 91)
     t_object.run_ranking()
 
 
 def func9():
-    t_object = Test(8, 9)
+    t_object = Test(91, 98)
     t_object.run_ranking()
-
-
-def func10():
-    t_object = Test(9, 10)
-    t_object.run_ranking()
-
 
 if __name__ == '__main__':
     p1 = Process(target=func1)
@@ -71,5 +65,3 @@ if __name__ == '__main__':
     p8.start()
     p9 = Process(target=func9)
     p9.start()
-    p10 = Process(target=func10)
-    p10.start()
--- tc_merge_files.py
+++ tc_merge_files.py
@@ -2,16 +2,15 @@
 Note: This is a static code written only for the purposes of testing 7 million on server
 For actual file generation refer to regular parameter taking files like tc_rerank_document_framework.py ( Git readme )
 """
-
+import os
 iterator_duo = 0
 
 name_list = []
-i = 0
-while i != 15:
-    name_list.append("partial_files/" + "result_set" + str(i) + ".run")
-    i += 1
+paths = os.listdir('partial_files')
+for path in paths:
+    name_list.append('partial_files/' + path)
 
-with open("combined_result_set.run", 'w') as outfile:
+with open("train.run", 'w') as outfile:
     for fname in name_list:
         with open(fname) as infile:
             for line in infile:
