4a7c51c94455e05edc18d27f3d027c6c0db3884f
Gaurav Patil <gauravpatil93@outlook.com>
2017-05-07 21:26:12 -0400

    Test Files

--- MultiThreadedTestBM25.py
+++ MultiThreadedTestBM25.py
@@ -1,14 +1,14 @@
 from multiprocessing import Process
-from Test_Class import Test
+from TestClassBM25 import Test
 
 
 def func1():
-    t_object = Test(0, 2)
+    t_object = Test(0, 11)
     t_object.run_ranking()
 
 
 def func2():
-    t_object = Test(2, 3)
+    t_object = Test(11, 21)
     t_object.run_ranking()
 
 
@@ -51,25 +51,24 @@ def func10():
     t_object = Test(91, 98)
     t_object.run_ranking()
 
-
 if __name__ == '__main__':
     p1 = Process(target=func1)
     p1.start()
     p2 = Process(target=func2)
     p2.start()
-    # p3 = Process(target=func3)
-    # p3.start()
-    # p4 = Process(target=func4)
-    # p4.start()
-    # p5 = Process(target=func5)
-    # p5.start()
-    # p6 = Process(target=func6)
-    # p6.start()
-    # p7 = Process(target=func7)
-    # p7.start()
-    # p8 = Process(target=func8)
-    # p8.start()
-    # p9 = Process(target=func9)
-    # p9.start()
-    # p10 = Process(target=func10)
-    # p10.start()
+    p3 = Process(target=func3)
+    p3.start()
+    p4 = Process(target=func4)
+    p4.start()
+    p5 = Process(target=func5)
+    p5.start()
+    p6 = Process(target=func6)
+    p6.start()
+    p7 = Process(target=func7)
+    p7.start()
+    p8 = Process(target=func8)
+    p8.start()
+    p9 = Process(target=func9)
+    p9.start()
+    p10 = Process(target=func10)
+    p10.start()
